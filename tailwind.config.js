fedb0adf7dbea625e07baab2d27bfb5e5e0838ed
Muhammad Ahmed <muhammadahmed120192@gmail.com>
2024-01-14 21:03:01 +0500

    added dark/light modes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,33 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      // TITLE/TEXT/FONT COLOR
+      colors: {
+        lightText: "rgb(255, 255, 255)",
+        darkText: "rgb(35, 35, 35)",
+
+        navLight: "rgb(0, 133, 250)",
+        // navLight: "rgb(255, 255, 255)",
+        navDark: "rgb(75, 75, 75)",
+
+        tabLight: "rgb(255, 255, 255)",
+        tabDark: "rgb(45, 45, 45)",
+
+        // bgDarkGradient: "rgb(45, 45, 45)",
+      },
+      // BODY GRADIENT COLOR
+      backgroundImage: {
+        // bgLightGradient: "linear-gradient(to right, rgb(255, 255, 255) 10%, rgb(209, 216, 255) 100%)",
+        bgLightGradient: "linear-gradient(to right, rgb(250, 250, 250) 10%, rgb(245, 245, 245) 90%)",
+        // bgDarkGradient: "linear-gradient(to bottom right, rgb(74, 85, 104) 10%, rgb(26, 32, 44) 90%)",
+        bgDarkGradient: "linear-gradient(to right, rgb(25, 25, 25) 10%, rgb(35, 35, 35) 90%)"
+
+      }
+    },
     fontFamily: {
-      sans: ['Lato', 'sans-serif'], // Add your desired sans-serif font
-      serif: ['Open Sans', 'serif'], // Add your desired serif font (optional)
+      sans: ['Lato', 'sans-serif'],
+      serif: ['Open Sans', 'serif'],
     },
   },
   plugins: [],
